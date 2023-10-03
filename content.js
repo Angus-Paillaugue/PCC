4aa5c3fc6766dd2424044959293f507d406c3d48
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-10-03 21:34:42 +0200

    I did to f***ing i can't describe it

--- content.js
+++ content.js
@@ -1,66 +1,99 @@
-// Function for actually fetching currencies convertion rates from the api
-function fetchLive(callback) {
-    fetch("https://api.exchangerate-api.com/v4/latest/EUR").then(res => res.json()).then((data) => {
+// Function for actually fetching currencies conversion rates from the api
+function fetchLive(currencyToConvertTo, callback) {
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyToConvertTo}`).then(res => res.json()).then((data) => {
         chrome.storage.local.set({cache: data.rates, cacheTime: Date.now()}, function() {
             callback(data.rates);
         });
     });
 }
 
-// Checking currencies convertion rates in cache (calls fetchLive if cache-miss)
-function getRates(callback) {
+// Checking currencies conversion rates in cache (calls fetchLive if cache-miss)
+function getRates(currencyToConvertTo, callback) {
     chrome.storage.local.get(['cache', 'cacheTime'], function(items) {
-        if (items.cache && items.cacheTime && items.cacheTime) {
+        if (items?.cache && items?.cacheTime) {
             if (items.cacheTime > Date.now() - 3600*1000) {
                 return callback(items.cache);
             }
         }
     
-        fetchLive(callback);
+        fetchLive(currencyToConvertTo, callback);
     });
 }
 
-
 // Some regex magic to convert currencies inside strings
-function formatString(string, rates, currencies){
-    string = string.replaceAll("~", "");
-    for(const currency of currencies){
-        let regex = new RegExp(currency.symbolRegex);
-        string = string.replace(regex, (match) => {
-            return ((Number(match.slice(currency.trimLength).trim())*currency.multiplyBy)/rates[currency.currencySymbole]).toFixed(2)+" €";
-        });
+function formatString(inputString, rates, currencies, currencyToConvertTo) {
+    // Do some regex magic to extract all the currency matches inside the provided string
+    const regex = /([$€¥£₹₽₴₱₪₨₩₦₢₣₥₫₵]?)\s*(\d+(\.\d+)?)/g;
+    const matches = [...inputString.matchAll(regex)];
+
+    if (matches.length === 0) return inputString;
+
+    let modifiedString = '';
+    let lastIndex = 0;
+    // For each currency math inside the string
+    for (const match of matches) {
+        const currencySymbol = match[1] || undefined;
+        const originalNumber = parseFloat(match[2]) ?? undefined;
+        // Asserts the match contains a currency symbol and a numerical value inside
+        if(currencySymbol && originalNumber){
+            // Extract the currency (returns object from currencies.json) from the match
+            const currency = currencies.filter(el => el.symbol === currencySymbol)[0];
+            // If the matched currency is not the same as the one we are trying to convert to
+            if(currency.name !== currencyToConvertTo){
+                // Get the rate of th currency to convert it to the one selected to the user
+                const rate = rates[currency.name];
+                // Convert the amount in the match
+                const convertedNumber = isNaN(originalNumber) ? '' : (parseFloat(originalNumber) / rate).toFixed(2);
+                // Modifies the
+                modifiedString += inputString.substring(lastIndex, match.index-1) + convertedNumber + " " + currencies.filter(el => el.name === currencyToConvertTo)[0].symbol;
+                modifiedString = modifiedString.replaceAll(currency.name, "");
+                lastIndex = match.index + match[0].length;
+            }
+        }
     }
-    return string;
+    modifiedString += inputString.substring(lastIndex);
+
+    return modifiedString;
 }
 
 // Checking if toggle switch on popup is enabled
 chrome.storage.local.get(["status"], (status) => {
     if(status.status){
-        // Gathering the currencies converting rates (trying for cache and if miss fetching it from the api)
-        getRates((rates) => {
-            // Fetch custom data from ./currencies.json
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", chrome.runtime.getURL('currencies.json'));
-            xhr.onreadystatechange  = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    const currencies = JSON.parse(xhr.responseText);
-        
-                    // Getting all the text nodes and converting the currencies inside their text content every second
-                    setInterval(() => {
-                        const text = document.querySelectorAll("span, p, a, h1, h2, h3, h4, h5, h6, em, tr, ul, ol, tr, label, div, dd, li");
-                        text.forEach(textNode => {
-                            for (var i = 0; i < textNode.childNodes.length; ++i){
-                                if (textNode.childNodes[i].nodeType === Node.TEXT_NODE){
-                                    let nodeText = textNode.childNodes[i].textContent;
-                                    let formattedText = formatString(nodeText, rates, currencies);
-                                    textNode.childNodes[i].textContent = formattedText;
+        // Checking the currency to convert to
+        chrome.storage.local.get(["convertTo"], function(convertTo){
+            // if currency to convert to does not exists, default to USD
+            convertTo = convertTo?.convertTo ?? "USD";
+
+            // Gathering the currencies converting rates (trying for cache and if miss fetching it from the api)
+            getRates(convertTo, (rates) => {
+                // Fetch currencies data from ./currencies.json
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", chrome.runtime.getURL('currencies.json'));
+                xhr.onreadystatechange  = function() {
+                    // Checks if the currencies.json fetching has ended and can proceed
+                    if (this.readyState == 4 && this.status === 200) {
+                        const currencies = JSON.parse(xhr.responseText);
+            
+                        // Getting all the text nodes and converting the currencies inside their text content every second
+                        setInterval(() => {
+                            const text = document.querySelectorAll("span, p, a, h1, h2, h3, h4, h5, h6, em, tr, ul, ol, tr, label, div, dd, li");
+                            text.forEach(textNode => {
+                                // For all of the children inside the elements
+                                for (var i = 0; i < textNode.childNodes.length; ++i){
+                                    // Checks if the child is a text node
+                                    if (textNode.childNodes[i].nodeType === Node.TEXT_NODE){
+                                        // Extract the text, calls the format function and replace the text with the converted one
+                                        let nodeText = textNode.childNodes[i].textContent;
+                                        let formattedText = formatString(nodeText, rates, currencies, convertTo);
+                                        textNode.childNodes[i].textContent = formattedText;
+                                    }
                                 }
-                            }
-                        });
-                    }, 1000);
-                }     
-            }
-            xhr.send();
+                            });
+                        }, 1000);
+                    }     
+                }
+                xhr.send();
+            });
         });
     }
 });
--- popup.js
+++ popup.js
@@ -1,19 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
+// On popup load
+document.addEventListener('DOMContentLoaded', () => {
     var status_input = document.getElementById('status');
-    status_input.addEventListener('change', function(e) {
+    status_input.addEventListener('change', (e) => {
         e.currentTarget.checked;
         chrome.storage.local.set({ "status": e.currentTarget.checked });
         reloadTab();
     });
-    chrome.storage.local.get(["status"], function(status){
-        if(status.status){
-            status_input.checked = true;
-        }
+    chrome.storage.local.get(["status"], (status) => {
+        if(status.status) status_input.checked = true;
     });
+
+    // Fetches the currencies.json to append to the select menu on the popup
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", chrome.runtime.getURL('currencies.json'));
+    xhr.onreadystatechange  = function() {
+        // Checks if the currencies.json fetching has ended and can proceed
+        console.log(this);
+        if (this.readyState == 4 && this.status === 200) {
+            const currencies = JSON.parse(xhr.responseText);
+            const currencySelect = document.getElementById("convertTo");
+
+            chrome.storage.local.get(["convertTo"], (convertTo) => {
+                // For each currency in the currencies.json, appends option to the select menu of the popup
+                currencies.forEach(currency => {
+                    const option = document.createElement("option");
+                    option.value = currency.name;
+                    option.text = `${currency.name} - ${currency.symbol}`;
+                    currencySelect.appendChild(option);
+                    if(convertTo?.convertTo === currency.name){
+                        option.selected = "selected";
+                    }else if(!convertTo?.convertTo && currency.name === "USD"){
+                        option.selected = "selected";
+                    }
+                });
+            });
+        
+            // Updates the convertTo variable in the chrome storage 
+            currencySelect.addEventListener('change', () => {
+                chrome.storage.local.set({ "convertTo": currencySelect.value });
+                reloadTab();
+            });
+        }     
+    }
+    xhr.send();
 });
 
-function reloadTab() {
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
-        chrome.tabs.reload(tab.id);
-    });
+// Get the tab with specified id (current tab if not specified) and reloads it
+const reloadTab = (id) => {
+    if(id){
+        chrome.tabs.reload(tab);
+    } else {
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([tab]) => {
+            chrome.tabs.reload(tab.id);
+        });
+    }
 }
