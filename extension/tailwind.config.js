fd7c2b284d23eb4ef187060153316029262f7341
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-11-09 21:22:52 +0100

    UI changes

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,4 +1,22 @@
-export default {
+// export default {
+//   content: ['./popup.html', './**/*.{js,ts,html}'],
+//   theme: {
+//     extend: {
+//       fontFamily:{
+//         sans:["Poppins"]
+//       },
+//       colors: {
+//         primary: "#11ba66",
+//         "text-main":"#2D2E32"
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+const withMT = require("@material-tailwind/html/utils/withMT");
+ 
+module.exports = withMT({
   content: ['./popup.html', './**/*.{js,ts,html}'],
   theme: {
     extend: {
@@ -12,4 +30,4 @@ export default {
     },
   },
   plugins: [],
-};
+});
