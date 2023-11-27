ec0331d698a6e15729ce791e97c91880af03ca63
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-11-27 15:10:42 +0100

    Hopefully fixed the redirects

--- website/src/routes/dashboard/admin/charts/+page.server.js
+++ website/src/routes/dashboard/admin/charts/+page.server.js
@@ -3,7 +3,7 @@ import { usersRef } from "$lib/server/db";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
-    if(!locals?.user) throw redirect(307, "/auth?redirect=/dashboard/charts ");
+    if(!locals?.user) throw redirect(307, "/auth?redirect=/dashboard/admin/charts ");
     if(!locals?.user?.isAdmin) throw redirect(307, "/dashboard");
 
     const users = await usersRef.find({  }).sort({ joined:-1 }).project({ _id:0 }).toArray();
