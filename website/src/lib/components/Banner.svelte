b6aef840c8651bd8e0179de5f65fd2ac97e23537
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2023-12-04 20:00:56 +0100

    Fixed banner

--- website/src/lib/components/Banner.svelte
+++ website/src/lib/components/Banner.svelte
@@ -26,7 +26,7 @@
     <div class="fixed top-16 left-0 bg-primary-600 dark:bg-primary-700 w-full h-12 z-40">
         <div class="max-w-screen-lg w-full mx-auto flex flex-row items-center justify-center h-full relative">
             <p class="text-neutral-100">{banner.message}</p>
-            <CloseButton  on:click={removeBanner} class="absolute top-1 right-1" />
+            <CloseButton  on:click={removeBanner} class="absolute top-1/2 -translate-y-1/2 right-1" />
         </div>
     </div>
 {/if}
